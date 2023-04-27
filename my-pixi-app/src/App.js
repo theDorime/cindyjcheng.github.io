8ef9abeff041d798006f29132e4497c262367621
theDorime <cjcheng@buffalo.edu>
2023-04-27 01:36:37 -0400

    bunny test

--- my-pixi-app/src/App.js
+++ my-pixi-app/src/App.js
@@ -1,32 +1,26 @@
 import * as PIXI from 'pixi.js';
 import React, { useLayoutEffect, useRef } from 'react';
+import { BlurFilter } from 'pixi.js';
+import { Stage, Container, Sprite, Text } from '@pixi/react';
+import { useMemo } from 'react';
 
 function App() {
-    const appRef = useRef(null);
-  
-    useLayoutEffect(() => {
-      const app = new PIXI.Application({
-        width: window.innerWidth,
-        height: window.innerHeight
-        //backgroundColor: 0x000000 // Set the background color to black
-      });
-      
-      appRef.current.appendChild(app.view);
-  
-      // // Create the sprite and add it to the stage
-      // let sprite = PIXI.Sprite.from('sample.png');
-      // app.stage.addChild(sprite);
+  const blurFilter = useMemo(() => new BlurFilter(4), []);
 
-      // // Add a ticker callback to move the sprite back and forth
-      // let elapsed = 0.0;
-      // app.ticker.add((delta) => {
-      //   elapsed += delta;
-      //   sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-      // })
+  return (
+    <Stage>
+      <Sprite
+        image="https://pixijs.io/pixi-react/img/bunny.png"
+        x={400}
+        y={270}
+        anchor={{ x: 0.5, y: 0.5 }}
+      />
 
-    }, []);
-  
-    return <div ref={appRef}></div>;
+      <Container x={400} y={330}>
+        <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} filters={[blurFilter]} />
+      </Container>
+    </Stage>
+  );
   }
   
   export default App;
--- my-pixi-app/src/index.js
+++ my-pixi-app/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BlurFilter } from 'pixi.js';
+import { Stage, Container, Sprite, Text } from '@pixi/react';
+import { useMemo } from 'react';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
 
-
-
+ReactDOM.render(<App />, document.body);
