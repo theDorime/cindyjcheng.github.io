e394566d2e09c4f6bfe5814a42fd462da52fda05
theDorime <cjcheng@buffalo.edu>
2023-04-27 23:26:22 -0400

    Testing react deployment only

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import * as PIXI from 'pixi.js';
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const appRef = useRef(null);
+  const [count, setCount] = useState(0);
 
-    useLayoutEffect(() => {
-      const app = new PIXI.Application({
-        width: window.innerWidth,
-        height: window.innerHeight,
-        backgroundColor: 0xD6D2C4 // Set the background color to black
-      });
-
-      // // Create the sprite and add it to the stage
-      // let sprite = PIXI.Sprite.from('cindyjcheng.github.io/public/dog.jpg');
-      // app.stage.addChild(sprite);
-
-      // // Add a ticker callback to move the sprite back and forth
-      // let elapsed = 0.0;
-      // app.ticker.add((delta) => {
-      //   elapsed += delta;
-      //   sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-      // })
-      
-      appRef.current.appendChild(app.view);
-      
-    }, []);
-
-    return <div ref={appRef}></div>;
+  function incrementCount() {
+    setCount(count + 1);
   }
+
+  return (
+    <div>
+      <h1>Hey!</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={incrementCount}>Click me</button>
+    </div>
+  );
+}
   
 export default App;
