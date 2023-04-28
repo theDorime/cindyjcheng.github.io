6b2ff31e3728a1f5ef7c3d876d21680fc04f45e7
theDorime <48629012+theDorime@users.noreply.github.com>
2023-04-27 21:14:40 -0400

    Delete my-pixi-app directory

--- my-pixi-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import { BlurFilter } from 'pixi.js';
-import { Stage, Container, Sprite, Text } from '@pixi/react';
-import { useMemo } from 'react';
-import App from './App';
-
-
-ReactDOM.render(<App />, document.body);
