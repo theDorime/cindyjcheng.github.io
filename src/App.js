3074749e08b7d4e72be1813311fe349087fe3940
theDorime <cjcheng@buffalo.edu>
2023-04-30 16:29:16 -0400

    Removed code

--- src/App.js
+++ src/App.js
@@ -2,19 +2,9 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
-
-  function incrementCount() {
-    setCount(count + 1);
-  }
-
   return (
     <div>
-      
       <h1>Hey!</h1>
-      <p>You clicked {count} times</p>
-      <button onClick={incrementCount}>Click me</button>
-
     </div>
   );
 }
