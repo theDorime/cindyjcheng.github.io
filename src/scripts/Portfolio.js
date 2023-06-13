16e603931ee67cccd82a0655e8567a5510b3cd68
theDorime <cjcheng@buffalo.edu>
2023-06-13 05:04:12 -0400

    Tested

--- src/scripts/Portfolio.js
+++ src/scripts/Portfolio.js
@@ -3,7 +3,7 @@ import { SideNavBar } from "./Util";
 export function Portfolio() {
    return <div>
         {SideNavBar('Portfolio')};
-        <h3> For my resume and stuff <br/>
+        <h3> For my resume and stuff <br/><br/>
         <a href="https://github.com/theDorime/cindyc">
             <button type="button" class="btn btn-dark">Github Repo</button>
         </a>
