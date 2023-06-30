e1e1615d6d23778ee04862664495d40a0b5aa1ec
theDorime <cjcheng@buffalo.edu>
2023-06-30 00:51:06 -0400

    auto build elements and change view on click and return to large view on double click

--- src/scripts/Gallery.js
+++ src/scripts/Gallery.js
@@ -1,160 +1,55 @@
 import { SideNavBar } from './Util'
 import '../styles/Gallery.css'
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { Navbar, Nav, Dropdown, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-export function ImageUpload(images) { 
-    return (
-        <div className="row">
-        <div class="col-lg-4 mb-4 mb-lg-0">
-            <div className="thumbnail">
-                <img src={ require('../assets/sample/img1.JPG') } className="img-fluid" alt="pic should be here" border-radius="10px"/>
-                <p>Photo of Niagara Falls.</p>
-    
-            </div>
-        </div>
-    </div>
-    )
-  };
-
-
 export function Gallery() {
+    const [isImageClicked, setImageClicked] = useState(false);
+    let imgCt = 0; 
 
-    const divRef1 = useRef(null);
-    const divRef2 = useRef(null);
-    const divRef3 = useRef(null);
+    const handleImageClick = () => {
+      if (!isImageClicked) {
+        setImageClicked(true);
+      } else {
+        setImageClicked(false);
+      }
+    };
+
+    const data = require('./gallery.json');
+    //const folderPath = "../assets/filmPhotos/"
+    const buildGallery = []
     
-    const scrollToDiv1 = () => {
-      divRef1.current.scrollIntoView({ behavior: "smooth" });
-    };
-    const scrollToDiv2 = () => {
-        divRef2.current.scrollIntoView({ behavior: "smooth" });
-    };
-    const scrollToDiv3 = () => {
-        divRef3.current.scrollIntoView({ behavior: "smooth" });
-    };
 
-    const navigate = useNavigate();
-    const links = [ 
-        { path: '/', text: 'Home' }, 
-        { path: '/Portfolio', text: 'Portfolio' }, 
-        { path: '/Game', text: 'Game' }
-    ]
-
-    const customToggleStyle = {
-        backgroundColor: 'transparent',
-        border: 'none',
-        color: 'inherit',
-        cursor: 'pointer',
-        textDecoration: 'none',
-        outline: 'none',
-        boxShadow: 'none',
-        fontWeight: 'bold'
+    data.forEach((section) => {
+      buildGallery.push(
+      <div className="line-with-header">
+          <h2 className="header-title">{section.name}</h2>
+          <hr className="line"/>
+      </div>)
+      
+      for (let i = 0; i < section.photos.src.length - (section.photos.src.length % 3); i++) {
+        const buildImgs = [];
+        if(i%3 == 0){
+          for (let ii = 0; ii < 3; ii++) {
+            buildImgs.push(
+              <img src={require(`../assets/filmPhotos/${section.path}${section.photos.src[i+ii]}`)} alt="Pic should be here" key={imgCt}
+              onClick={handleImageClick}
+              />
+            )
+            imgCt++; 
+          }
+          buildGallery.push(<div className={isImageClicked ? "image-gallery-clicked" : 'image-gallery'}key={i}>{buildImgs}</div>);
+        };
       };
-
-      const itemStyle = {
-        backgroundColor: 'transparent',
-        border: 'none',
-        color: 'inherit',
-        cursor: 'pointer',
-        textDecoration: 'none',
-        outline: 'none',
-        boxShadow: 'none',
-        
-      };
-
-      const pr = '../assets/filmPhotos/img'
-    return <div>
-         
-      <Navbar className="navBar ml-auto" fixed="top" >
-          <Nav> {links.map((link, index) => (
-              <Nav.Link onClick={() => navigate(link.path)}>{link.text}</Nav.Link>))}
-          </Nav>
-          <Dropdown>
-              <Dropdown.Toggle style={customToggleStyle}>
-                  Gallery
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Button style={itemStyle} onClick={scrollToDiv1}> Puerto Rico </Button>
-                <Button style={itemStyle} onClick={scrollToDiv2}> Graduation </Button>
-                <Button style={itemStyle} onClick={scrollToDiv3}> Niagara Falls </Button>
-              </Dropdown.Menu>
-      </Dropdown>
-  </Navbar>
-  
-        <div class="line-with-header">
-            <h2 class="header-title">Puerto Rico</h2>
-            <hr class="line"/>
-        </div>
-        <div ref={divRef1} className="image-gallery">
+    });
     
-            <img src={ require('../assets/filmPhotos/puertoRico/img2.JPG') } alt="pic should be here"/>
-
-            <img src={ require('../assets/filmPhotos/puertoRico/img3.JPG') }  alt="pic should be here"/>
-            
-            <img src={ require('../assets/filmPhotos/puertoRico/img4.JPG') } alt="pic should be here"/>
-            
-        </div>
-
-        <div className="image-gallery">
-     
-        <img src={ require('../assets/filmPhotos/puertoRico/img5.JPG') } alt="pic should be here"/>
-
-        <img src={ require('../assets/filmPhotos/puertoRico/img1.JPG') }  alt="pic should be here"/>
-
-        <img src={ require('../assets/filmPhotos/puertoRico/img6.JPG') } alt="pic should be here"/>
-            
-        </div>
-
-        <div className="image-gallery">
-     
-        <img src={ require('../assets/filmPhotos/puertoRico/img7.JPG') } alt="pic should be here"/>
-
-        <img src={ require('../assets/filmPhotos/puertoRico/img8.JPG') }  alt="pic should be here"/>
-
-        <img src={ require('../assets/filmPhotos/puertoRico/img9.JPG') } alt="pic should be here"/>
-            
-         </div>
-
-    
-
-         <div ref={divRef2} class="line-with-header">
-            <h2 class="header-title">Graduation</h2>
-            <hr class="line"/>
-        </div>
-        <div className="image-gallery">
-     
-            <img src={ require('../assets/filmPhotos/grad/img1.JPG') } alt="pic should be here"/>
-
-            <img src={ require('../assets/filmPhotos/grad/img2.JPG') }  alt="pic should be here"/>
-            
-            <img src={ require('../assets/filmPhotos/grad/img3.JPG') } alt="pic should be here"/>
-            
-        </div>
-
-        <div className="image-gallery">
-     
-            <img src={ require('../assets/filmPhotos/grad/img4.JPG') } alt="pic should be here"/>
-            <img src={ require('../assets/filmPhotos/grad/img5.JPG') } alt="pic should be here"/>
-            <img src={ require('../assets/filmPhotos/grad/img6.JPG') } alt="pic should be here"/>
-            
-        </div>
-
-        <div ref={divRef3} class="line-with-header">
-            <h2 class="header-title">Niagara Falls</h2>
-            <hr class="line"/>
-        </div>
-        <div className="image-gallery">
-     
-            <img src={ require('../assets/sample/img1.JPG') } alt="pic should be here"/>
-
-            <img src={ require('../assets/sample/img2.JPG') }  alt="pic should be here"/>
-            
-            <img src={ require('../assets/sample/img3.JPG') } alt="pic should be here"/>
-            
-        </div>
-        </div>
- 
+      // for (let i = 0; i < (section.photos.src.length % 3) + section.photos.src.length % 3; i++) { 
+      //   const buildPH = []; 
+      //   buildPH.push(<div className="image-placeholder"/>)
+      // };
+  return <div>
+    {SideNavBar('Gallery')}
+    {buildGallery}
+    </div>
 };
