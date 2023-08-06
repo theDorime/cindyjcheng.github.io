165b17406380c3890a6d8c08459bb6e7a59deef5
theDorime <cjcheng@buffalo.edu>
2023-08-06 19:51:58 -0400

    homepage

--- src/scripts/Home.js
+++ src/scripts/Home.js
@@ -5,20 +5,14 @@ export function Home() {
     return <div> 
         {SideNavBar('Home')}
         <div class="container-fluid bg-2 text-center">
-            <img src={require("./pfp.JPG")} class="img-responsive img-circle margin" alt="Bird" width="350" height="350"/>
+            <img src={require("./pfp.JPG")} class="img-responsive img-circle margin" alt="Bird" width="300" height="300"/>
             <h3 className="aboutText">
                 Welcome and thank you for stopping by my personal website. 
-<br></br>
-                I was born and raised in Chinatown NY and graduated from University at Buffalo 2023 in Computer Science with a minor in Philosophy. 
-                Aside from working as a Software Dev with a focus in Cloud and Metadata development, I enjoy learning about web and game dev 
-                to pursue creative projects! Encapsulating my passion in Computer Science is aspiring to learn, adapt, and apply creative problem solving in 
-                life. Such as my love for rock climbing- solving a beta through trial and error, learning other approaches/techniques from peers, and embracing falling. 
+                <br></br>
                 I created this site to play around with code, share, and inspire! 
-
-                Lots of love, 
-                Cindy J Cheng.
             </h3>
         </div>
+        
     </div>
 };
 
