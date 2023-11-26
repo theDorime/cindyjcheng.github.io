474bb90c5e3a22adfee211971c89dc8b3ccb9b8b
theDorime <cjcheng@buffalo.edu>
2023-11-25 23:40:55 -0500

    Hybrid database for metadata and json generator setup

--- src/server/json_images.py
+++ src/server/json_images.py
@@ -1,5 +1,29 @@
 import json
+import mysql.connector
 
+connection = mysql.connector.connect(host='localhost', user='root', password='', database='Gallery')
+
+if connection.is_connected(): 
+    print('Connected Successfully') 
+
+     # Create a cursor to execute SQL queries
+    cursor = connection.cursor()
+
+    # Example query: Retrieve all rows from a table
+    query = "SELECT * FROM graduation"
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Process the rows
+    for row in rows:
+        print(row)
+else: 
+    print('Connection Failed')
+
+connection.close()
 data = {
     "name": "John Doe",
     "age": 30,
