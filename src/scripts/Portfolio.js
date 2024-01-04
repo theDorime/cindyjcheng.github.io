083176c1eaa98702a207ed1b4b875eec4825a5b6
theDorime <cjcheng@buffalo.edu>
2024-01-04 00:47:29 -0500

    Adding resume

--- src/scripts/Portfolio.js
+++ src/scripts/Portfolio.js
@@ -6,14 +6,22 @@ export function Portfolio() {
 
         <h3 className='portAlign'> Info <br/><br/>
 
-        
-        <a href="https://github.com/theDorime/cindyc">
-            <button type="button" class="btn btn-dark">Github Repo</button>
+        <a href="https://cindyjcheng.github.io\src\assets\Resume.pdf" target="_blank">
+            <button type="button" class="btn btn-dark">Resume</button>
         </a>
+        
         <br/><br/>
+
         <a href="https://www.linkedin.com/in/cindy-j-c-566615167">
             <button type="button" class="btn btn-dark">Linkedin</button>
         </a>
+
+        <br/><br/>
+
+        <a href="https://github.com/theDorime/cindyc">
+            <button type="button" class="btn btn-dark">Site's Github Repo</button>
+        </a>
+        
         </h3>
     </div>
 };
