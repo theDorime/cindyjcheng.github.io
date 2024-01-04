370e37b17b565b944df945ac81210ad8696615da
theDorime <cjcheng@buffalo.edu>
2024-01-04 01:08:45 -0500

    Btn to download resume

--- src/scripts/Portfolio.js
+++ src/scripts/Portfolio.js
@@ -1,13 +1,27 @@
 import { SideNavBar } from './Util';
 import '../styles/Portfolio.css';
 export function Portfolio() {
+
+    const resumeDownload = () => {
+        //update pdf path 
+        const pdfUrl = 'https://github.com/theDorime/cindyc/raw/gallery-database/src/assets/Resume.pdf';
+
+        const link = document.createElement('a');
+        link.href = pdfUrl;
+        link.style.display = 'none';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+      };
+
    return <div>
         {SideNavBar('Portfolio')}
 
         <h3 className='portAlign'> Info <br/><br/>
 
-        <a href="https://cindyjcheng.github.io\src\assets\Resume.pdf" target="_blank">
-            <button type="button" class="btn btn-dark">Resume</button>
+        <a herf="https://github.com/theDorime/cindyc/raw/gallery-database/src/assets/Resume.pdf">
+            <button type="button" class="btn btn-dark" onClick={resumeDownload}>Resume</button>
         </a>
         
         <br/><br/>
